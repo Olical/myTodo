11b6843546845103aec5893dd70d06fe2aa14308
Oliver Caldwell <olliec87@gmail.com>
2012-09-13 20:20:26 +0100

    Created the Todo model.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,23 @@
 $(function() {
+    /*global Backbone:true*/
+
     'use strict';
+
+    var Todo = Backbone.Model.extend({
+        defaults: function() {
+            return {
+                title: 'nothing to do here...',
+                created: new Date().getTime(),
+                done: false
+            };
+        },
+
+        initialize: function() {
+            if(!this.get('title')) {
+                this.set({
+                    title: this.defaults.title
+                });
+            }
+        }
+    });
 });
