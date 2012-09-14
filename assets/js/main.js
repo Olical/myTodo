40efd482ef4b7f83794ac425cedd5dde419e5ae4
Oliver Caldwell <olliec87@gmail.com>
2012-09-14 11:22:18 +0100

    Added a comment about DOM ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,7 @@ define([
     /*global Backbone:true,Store:true,_:true*/
     'use strict';
 
+    // Wait until the DOM is ready
     $(function() {
         // Initialise the app and get everything going
         var app = new AppView();
