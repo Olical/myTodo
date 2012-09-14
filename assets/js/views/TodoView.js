40af193c09d7457d820106aba12be4c738a12a5f
Oliver Caldwell <olliec87@gmail.com>
2012-09-14 12:01:59 +0100

    Commented up the TodoView

--- assets/js/views/TodoView.js
+++ assets/js/views/TodoView.js
@@ -11,20 +11,28 @@ define(function() {
         },
 
         initialize: function() {
+            // Rebuild the HTML if the models data changes
             this.model.bind('change', this.render, this);
+
+            // If the model is destroyed then remove the element
             this.model.bind('destroy', this.remove, this);
         },
 
         render: function() {
+            // Renders the element from the models data
+            // The data is cleaned by converting it to JSON
             this.$el.html(this.template(this.model.toJSON()));
             return this;
         },
 
         toggleDone: function() {
+            // Simply toggles the done flag on the model
+            // Because it fires the change event the element will be rendered
             this.model.toggleDone();
         },
 
         destroy: function() {
+            // Destroys the models data which causes the element to be removed
             this.model.destroy();
             return false;
         }
