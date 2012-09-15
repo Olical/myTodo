e4ec20c6d76bdcb7f4575384bfe6aecda89ee3cf
Oliver Caldwell <olliec87@gmail.com>
2012-09-15 12:05:36 +0100

    Added editing.

--- assets/js/views/TodoView.js
+++ assets/js/views/TodoView.js
@@ -1,13 +1,25 @@
 define(function() {
+    // This is the state enum
+    // It lets the application know what state the view is in
+    var states = {
+        normal: 0,
+        editing: 1
+    };
+
     // Individual list item todos
     // Renders to an actual <li> tag
     // Updates on change
     var TodoView = Backbone.View.extend({
         tagName: 'li',
         template: _.template($('#todo-template').html()),
+        editTemplate: _.template($('#edit-todo-template').html()),
+        state: states.normal,
         events: {
             'click .toggle': 'toggleDone',
-            'click .destroy': 'destroy'
+            'click .destroy': 'destroy',
+            'click .begin-edit': 'beginEdit',
+            'click .cancel-edit': 'cancelEdit',
+            'click .save-edit': 'saveEdit'
         },
 
         initialize: function() {
@@ -16,13 +28,47 @@ define(function() {
 
             // If the model is destroyed then remove the element
             this.model.bind('destroy', this.remove, this);
+
+            // Render the view when the state changes
+            this.bind('state:change', this.render, this);
         },
 
         render: function() {
             // Renders the element from the models data
-            // The data is cleaned by converting it to JSON
-            this.$el.html(this.template(this.model.toJSON()));
+            // It uses a different template depending on the state
+            if(this.state === states.normal) {
+                // The data is cleaned by converting it to JSON
+                this.$el.html(this.template(this.model.toJSON()));
+            }
+            else if(this.state === states.editing) {
+                this.$el.html(this.editTemplate(this.model.toJSON()));
+            }
+
             return this;
+        },
+
+        setState: function(state) {
+            // Sets a new state and triggers the event
+            this.state = state;
+            this.trigger('state:change');
+        },
+
+        beginEdit: function() {
+            // Swap into the editing state
+            this.setState(states.editing);
+        },
+
+        cancelEdit: function() {
+            // Drop out of the editing state without saving
+            this.setState(states.normal);
+        },
+
+        saveEdit: function() {
+            // Save and exit the editing state
+            this.model.save({
+                content: this.$('.edited').val()
+            });
+            this.cancelEdit();
         },
 
         toggleDone: function() {
